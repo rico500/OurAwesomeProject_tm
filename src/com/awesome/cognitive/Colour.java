5c6d7d8061b9e94421e5ffc7a91c4b0cef8a7d4b
DraigLlofrudd <draig@lokink.com>
2016-11-20 02:24:23 +0900

    I: Added basic Colour interpretation. Colours.generateColour(String) :: Takes in a json string from the Microsoft API and returns a Colour object.

---
+++ src/com/awesome/cognitive/Colour.java
@@ -0,0 +1,34 @@
+package com.awesome.cognitive;
+
+/**
+ * Created by ignat on 20/11/2016.
+ */
+public class Colour {
+
+    public int r, g, b, a;
+
+    public Colour(int r, int g, int b){
+        this.r = check(r);
+        this.g = check(g);
+        this.b = check(b);
+        this.a = 255;
+    }
+
+    public Colour(int r, int g, int b, int a){
+        this.r = check(r);
+        this.g = check(g);
+        this.b = check(b);
+        this.a = check(a);
+    }
+
+    private int check(int k){
+        return Math.max(Math.min(k, 255),0);
+    }
+
+    public void add(Colour c){
+        r = c.r;
+        g = c.g;
+        b = c.b;
+        a = (a + c.a) / 2;
+    }
+}
