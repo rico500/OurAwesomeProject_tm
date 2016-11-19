fdbb40e0a540fe0cd7140e5d31ecbb52868dd03f
rico500 <eric.mhr.brunner@gmail.com>
2016-11-19 19:31:21 +0100

    Emotion: parsed json and got colors, still working on exception

--- src/com/awesome/Main.java
+++ src/com/awesome/Main.java
@@ -7,30 +7,42 @@ import java.lang.Thread;
 
 import com.awesome.hardware.Keyboard;
 import com.awesome.hardware.WebcamSupport;
+import com.oracle.javafx.jmx.json.JSONException;
 
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+
         WebcamSupport.showWebCamLiveFeed();
         //Keyboard.initKeyboard();
 
         try {
 
             while (true) {
-                Thread.sleep(2000);
-                String s = Emotion.getEmotion(WebcamSupport.getSnapshot());
-                s = s.substring(1,s.length()-1);
-                System.out.println(s);
-                if(!s.equalsIgnoreCase("[]")){
-                    Colour c = Colours.generateColour(s);
+                    Thread.sleep(2000);
+                    String s = Emotion.getEmotion(WebcamSupport.getSnapshot());
+                    s = s.substring(1, s.length() - 1);
+                    System.out.println(s);
 
-                }
+                    if (!s.equalsIgnoreCase("")) {
 
-                //Keyboard.setColor(0, 255, 0);
+                        try {
+
+                            Colour c = Colours.generateColour(s);
+                            System.out.println(c.toString());
+
+                        }catch(JSONException e){
+                            System.err.println("Parsing error! JSON : " + s);
+                        }
+
+                    }
+
+                    //Keyboard.setColor(0, 255, 0);
             }
 
-        }catch(InterruptedException e){
+        }
+        catch(InterruptedException e){
                 System.out.print(e.getMessage());
             }
     }
