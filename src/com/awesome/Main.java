ad33691f5b15c17bfb73a4b153d1efe6ad40a501
Danil Ivanov <danilivanov.ivanov@gmail.com>
2016-11-19 16:35:47 +0100

    Merged hardware into master

--- src/com/awesome/Main.java
+++ src/com/awesome/Main.java
@@ -6,7 +6,7 @@ import com.awesome.hardware.WebcamSupport;
 public class Main {
 
     public static void main(String[] args) {
-
+        //Test
         WebcamSupport.getSnapshot();
         Keyboard myKeyboard = new Keyboard();
         myKeyboard.setColor(100,0,0);
