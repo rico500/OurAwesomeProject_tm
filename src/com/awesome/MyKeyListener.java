618da70587953f33de27c4b3f7b0661755b55556
Danil Ivanov <danilivanov.ivanov@gmail.com>
2016-11-20 06:26:30 +0100

    test of kry listner

--- src/com/awesome/MyKeyListener.java
+++ src/com/awesome/MyKeyListener.java
@@ -22,21 +22,22 @@ class MyKeyListener implements KeyListener {
     @Override
     public void keyPressed(KeyEvent e) {
         int keyPressedIndex = (int)(e.getKeyChar())-97;
+        byte foo = (byte) ((int) e.getKeyChar());
         byte keyCodes[] = {0x1E, 0x30, 0x2E, 0x20, 0x12, 0x21, 0x22, 0x23, 0x17, 0x24, 0x25, 0x26, 0x32, 0x31, 0x18, 0x19, 0x10, 0x13, 0x1F, 0x14, 0x16, 0x2F, 0x11, 0x2D, 0x15, 0x2C};
         System.out.println(keyPressedIndex);
 
         if (!pressedKeys.contains(e.getKeyChar())){
             pressedKeys.add(e.getKeyChar());
             new Thread(() -> {
-                LogiLED.LogiLedSaveLightingForKey(keyCodes[keyPressedIndex]);
-                LogiLED.LogiLedSetLightingForKeyWithKeyName(keyCodes[keyPressedIndex], 100, 0, 0);
+                LogiLED.LogiLedSaveLightingForKey((byte) ((int) e.getKeyChar()));
+                LogiLED.LogiLedSetLightingForKeyWithKeyName((byte) ((int) e.getKeyChar()), 100, 0, 0);
                 try{
                     Thread.sleep(750);
                 } catch(InterruptedException e1){
                     e1.printStackTrace();
                 }
                 pressedKeys.remove(e.getKeyChar());
-                LogiLED.LogiLedRestoreLightingForKey(keyCodes[keyPressedIndex]);
+                LogiLED.LogiLedRestoreLightingForKey((byte) ((int) e.getKeyChar()));
             }).start();
         }
     }
